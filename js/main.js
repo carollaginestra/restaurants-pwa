3b9e4b03e89aa39dc503465332ed467bf181f5b3
Carol <carolinalaginestra@hotmail.com>
2019-04-26 09:35:49 -0300

    adjusting semantic and accessibility Adding attribute role in breadcrumbs, using correct order of headings, removing clone from sw

--- js/main.js
+++ js/main.js
@@ -162,7 +162,7 @@ createRestaurantHTML = (restaurant) => {
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   div.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   name.setAttribute("tabindex", "0");
   content.append(name);
